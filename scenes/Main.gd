30df2fe92e73bee600f8b9d37f33c812b7165204
Jeff <pancakewagons@gmail.com>
2023-01-13 14:45:40 -0500

    Fixed tab/space issues. Made it single row
    Last reel is wrong most of the time.

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -6,11 +6,11 @@ func _ready():
   slot.connect("stopped", self, "_on_slot_machine_stopped")
 
 func _on_Roll_button_down():
-  if $Roll.text == "Roll":
-    slot.start()
-    $Roll.text = "Stop"
-  else:
-    slot.stop()
+	if $Roll.text == "Roll":
+		slot.start()
+		$Roll.text = "Stop"
+	else:
+		slot.stop()
 
 func _on_slot_machine_stopped():
   $Roll.text = "Roll"
